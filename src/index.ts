237452e71a715a005800333136f482fdc93b3c15
Guilherme Moraes <gmoraes@thoughtworks.com>
2019-05-31 18:45:23 -0300

    Provide more endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
 import express from 'express';
-import {RequisitionParser} from 'enqueuer/js/requisition-runners/requisition-parser';
-import {RequisitionRunner} from 'enqueuer/js/requisition-runners/requisition-runner';
-import {Store} from 'enqueuer/js/configurations/store';
-import {Configuration} from 'enqueuer/js/configurations/configuration';
-import {ReportModel} from 'enqueuer/js/models/outputs/report-model';
+import {
+    InputRequisitionModel,
+    IdGenerator,
+    Store,
+    Configuration,
+    DynamicModulesManager,
+    RequisitionRunner,
+    Notifications,
+    NotificationEmitter,
+    Logger,
+    RequisitionParser,
+    OutputRequisitionModel
+} from 'enqueuer';
 
 const app = express();
 
@@ -20,37 +28,63 @@ app.use((req: any, res: any, next: any) => {
     });
 });
 
-const reportsHistory: any = {};
+interface ReportsHistory {
+    [propName: string]: OutputRequisitionModel;
+}
 
-// Logger.setLoggerLevel('trace');
+const reportsHistory: ReportsHistory = {};
+// Logger.setLoggerLevel('info');
+
+NotificationEmitter.on(Notifications.REQUISITION_RAN, (notification: OutputRequisitionModel) => {
+    console.log(`Requisition #${notification.id} ran`);
+    reportsHistory[notification.id] = notification;
+});
+
 app.post('/requisitions', async (requisition: any, response: any) => {
-    const requisitionModel = new RequisitionParser().parse(requisition.rawBody);
+    const requisitionModel: InputRequisitionModel = new RequisitionParser().parse(requisition.rawBody);
     try {
-        const reports = await new RequisitionRunner(requisitionModel).run();
-        reports.forEach((item: ReportModel) => reportsHistory[item.id] = item);
-        response.send(reports);
+        let id: string = requisitionModel.id;
+        if (requisitionModel.id === undefined) {
+            id = new IdGenerator(requisitionModel).generateId();
+            requisitionModel.id = id;
+        }
+        new RequisitionRunner(requisitionModel).run();
+        response.status(200).send({id});
     } catch (e) {
         response.status(400).send(e);
     }
 });
 
-app.get('/reports/:id', async (requisition: any, response: any) => {
+app.get('/requisitions/:id', (requisition: any, response: any) => {
     const report = reportsHistory[requisition.params.id];
     response.status(report !== undefined ? 200 : 400).send(report);
+});
+
+app.get('/requisitions', (requisition: any, response: any) => {
+    response.status(200).send(Object.keys(reportsHistory));
 });
 
 app.get('/store', async (requisition: any, response: any) => {
     response.send(Store.getData());
 });
 
-app.post('/plugins', async (requisition: any, response: any) => {
+app.post('/plugins', (requisition: any, response: any) => {
+    const result: any = {};
     const pluginsList = JSON.parse(requisition.rawBody);
-    pluginsList.forEach((plugin: string) => Configuration.getInstance().addPlugin(plugin));
-    response.status(200).send();
+    pluginsList.forEach((plugin: string) => result[plugin] = Configuration.getInstance().addPlugin(plugin));
+    response.status(200).send(result);
 });
 
-app.get('/configurations', async (requisition: any, response: any) => {
-    response.send(Configuration.getInstance());
+app.get('/plugins', (requisition: any, response: any) => {
+    response.status(200).send(Configuration.getInstance().getPlugins());
+});
+
+app.get('/loadedModules', (requisition: any, response: any) => {
+    response.status(200).send(DynamicModulesManager.getInstance().getLoadedModules());
+});
+
+app.get('/configurations', (requisition: any, response: any) => {
+    response.send(Configuration.getInstance().getValues());
 });
 
 let port = 3000;
